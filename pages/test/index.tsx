33d3c4d20ad053971946fe670f2e579a2e104b78
momthu <thumomm10@gmail.com>
2023-11-21 07:57:40 +0700

    upload

---
+++ pages/test/index.tsx
@@ -0,0 +1,9 @@
+import { FileUpload } from "../component/UploadPDF";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-100">
+      <FileUpload />
+    </main>
+  );
+}
